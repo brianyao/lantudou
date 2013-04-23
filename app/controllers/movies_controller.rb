31bdb1615516bf0139066ad0363ecde261834b06
BrianYao <sc.xiong@qq.com>
2013-04-23 14:13:03 -0700

    #1 enhan: fixed hilite

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,8 +6,9 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index    
-    @movies = Movie.order(params[:sort]).all
+  def index
+    @sort = params[:sort]    
+    @movies = Movie.order(@sort).all
   end
 
   def new
