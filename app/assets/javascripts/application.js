a96aa928faafc3e140e1ec57f884dedc28922c7d
BrianYao <sc.xiong@qq.com>
2013-05-27 06:28:05 -0700

    first half of the JavaScript tutorial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,30 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+RP = {
+    setup: function() {
+        // construct new DOM elements
+        $('<label for="filter" class="explanation">' +
+          'Restrict to movies suitable for children' +
+          '</label>' +
+          '<input type="checkbox" id="filter"/>'
+         ).insertBefore('#movies').change(RP.filter_adult);
+    },
+    filter_adult: function () {
+        // 'this' is element that received event (checkbox)
+        if ($(this).is(':checked')) {
+            $('#movies tr.adult').hide();
+            //$('#movies tbody tr').each(RP.hide_if_adult_row);
+        } else {
+            $('#movies tbody tr').show();
+        };
+    },
+    // hide_if_adult_row: function() {
+    //     if (! /^G|PG$/i.test($(this).find('td:nth-child(2)').text())) {
+    //         $(this).hide();
+    //     }
+    // }
+}
+$(RP.setup);       // when document ready, run setup code
